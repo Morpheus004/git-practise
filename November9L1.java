6e9ba1383515868869485760e5ac789146623329
Morpheus004 <prathambhanushali2004@gmail.com>
2023-11-17 12:08:10 +0530

    Add file

---
+++ November9L1.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+
+class Solution
+{
+    //Function to return the level order traversal of a tree.
+    static ArrayList <Integer> levelOrder(Node node) 
+    {
+        // Your code here
+        ArrayList<Integer> O=new ArrayList<>();
+        Queue<Node> Q=new LinkedList<>();
+        
+        Q.add(node);
+        while(!Q.isEmpty())
+        {
+            Node N=Q.remove();
+            O.add(N.data);
+            if(N.left==null||N.right==null);
+            if(N.left!=null)Q.add(N.left);
+            if(N.right!=null)Q.add(N.right);
+        }
+        return O;
+    }
+}
---
+++ November9L2.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+// import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class November9L2 {
+    
+}
+class Tree
+{
+    public ArrayList<Integer> reverseLevelOrder(Node node) 
+    {
+        // code here
+        ArrayList<Integer> O=new ArrayList<>();
+        Queue<Node> Q=new LinkedList<>();
+        
+        Q.add(node);
+        while(!Q.isEmpty())
+        {
+            Node N=Q.remove();
+            O.add(0,N.data);
+            if(N.left==null||N.right==null);
+            if(N.right!=null)Q.add(N.right);     //normal traversal me left pehle tha fir right tha isme right pehle insert kar diyaa
+            if(N.left!=null)Q.add(N.left);
+            
+        }
+        // return O;
+        // Collections.reverse(O);
+        return O;
+    }
+}
